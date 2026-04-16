a849ff0531023e31ab351fcf0131b9576c7102e1
Ali Moradi <ali.mrd318@gmail.com>
2026-04-16 03:41:54 +0330

    fix

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ import path from "path";
 const nextConfig: NextConfig = {
   output: "standalone",
   outputFileTracingRoot: path.resolve(__dirname),
+  images: {
+    unoptimized: true,
+  },
   webpack: (config) => {
     config.resolve.alias = {
       ...(config.resolve.alias || {}),
